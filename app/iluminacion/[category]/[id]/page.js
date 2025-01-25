97e5e1bbe945052329bff734b8821c25b97e9cd4
Franco Ursino <franncoursinno@gmail.com>
2025-01-25 08:37:09 -0300

    [add] Add Iluminacion products and pages

---
+++ app/iluminacion/[category]/[id]/page.js
@@ -0,0 +1,25 @@
+import { notFound } from "next/navigation";
+import ItemDetailHeader from "@/components/ItemDetailHeader";
+import ItemDetailMain from "@/components/ItemDetailMain";
+import ItemDetailDetails from "@/components/ItemDetailDetails";
+import { GetProductsById } from "@/actions/GetProductById";
+
+const IluminacionIdPage = async ({ params }) => {
+  const { id } = params;
+  const product = await GetProductsById(id);
+
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <div className="min-h-screen text-white">
+      <ItemDetailHeader category="iluminacion" />
+      <ItemDetailMain imageSrc={product.image1} name={product.name} type={product.type}
+      />
+      <ItemDetailDetails product={product} />
+    </div>
+  );
+};
+
+export default IluminacionIdPage;
--- app/iluminacion/[category]/page.js
+++ app/iluminacion/[category]/page.js
@@ -2,7 +2,7 @@ import Banner from "@/components/ui/Banner"
 import { getProducts } from "@/actions/GetProducts"
 import ItemList from "@/components/ItemList"
 
-export default async function DecoCategoryPage({ params }) {
+async function IluminacionCategoryPage({ params }) {
 
   const { category } = await params
   const { payload: products } = await getProducts(category)
@@ -14,3 +14,5 @@ export default async function DecoCategoryPage({ params }) {
     </>
   )
 }
+
+export default IluminacionCategoryPage
